1a187b5b796eb472b96591feac91392c974ec171
Rohit Mishra <rohit.mishra@mindcrewtech.com>
2024-07-06 13:06:35 +0530

    vercel-server-config

--- server/main.js
+++ server/main.js
@@ -1,15 +1,22 @@
 import applicationStack from "./app.js";
 
-const {
-    attachCoreMiddlewares,
-    attachExternalMiddlewares,
-    attachRouters,
-    upServer,
-  } = applicationStack,
-  bootstrap = async () => {
-    await attachCoreMiddlewares();
-    await attachExternalMiddlewares();
-    await attachRouters();
-    await upServer();
-  };
-await bootstrap();
+const { attachCoreMiddlewares, attachExternalMiddlewares, attachRouters, app } =
+  applicationStack;
+
+async function bootstrap() {
+  await attachCoreMiddlewares();
+  await attachExternalMiddlewares();
+  await attachRouters();
+}
+
+// This function will be called by Vercel
+export default async function (req, res) {
+  // Only run bootstrap once
+  if (!app.bootstrapped) {
+    await bootstrap();
+    app.bootstrapped = true;
+  }
+
+  // Handle the request
+  app(req, res);
+}
